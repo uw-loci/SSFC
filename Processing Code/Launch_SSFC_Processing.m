6b92d4bcc953763dfbf027a4f10384d65d11d93c
Niklas Gahm <niklas.gahm@gmail.com>
2018-11-29 12:37:29 -0600

    Uploading the Processing Launcher
    This is just the simple launcher script that abstracts away the all the underlying functions and scripts.

---
+++ Processing Code/Launch_SSFC_Processing.m
@@ -0,0 +1,38 @@
+function [ ] = Launch_Spectrally_Split_Swept_Field_Confocal_Processing( )
+%% Spectrally-Split Swpet Field Confocal Processing Launcher
+%   This launches the spectrally-split swept ffield confocal processing GUI
+% 
+%     Copyright (C) 2018 Niklas Gahm
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%   2018/08/01 - Started
+%   2018/08/01 - Finished
+
+
+
+%% Get Navigation Hook
+hpath = pwd;
+
+
+%% Add Required Path
+addpath('./Functions');
+
+
+%% Launch GUI Function
+SSFC_GUI; 
+
+
+%% Return to Starting Point
+cd(hpath);
+
+end
+
